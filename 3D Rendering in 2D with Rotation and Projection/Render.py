e0231798a6d0e1c8b081ead9c1a8edebb74c5ad3
Mateusz <msumiec@gmail.com>
2019-05-01 11:18:48 +0200

    Rotation Y and Z works

--- 3D Rendering in 2D with Rotation and Projection/Render.py
+++ 3D Rendering in 2D with Rotation and Projection/Render.py
@@ -18,7 +18,7 @@ class Render():
         self.screen = pg.display.set_mode((width, height))
         self.screen.fill(darkBlue)
 
-        self.vertices = cube * 50
+        self.vertices = cube * 100
 
         self.thetaX = 0
         self.thetaY = 0
@@ -44,21 +44,35 @@ class Render():
         From linear algebra we get:
         (A * B^T)^T = B^T^T * A^T = B * A^T
         """
-        return np.matmul(vertex, self.create_rotation_matrix_X(self.thetaX))
+        return np.matmul(vertex, self.create_rotation_matrix_X(self.thetaX).T)
 
     def create_rotation_matrix_Y(self, theta=0.0):
-        self.rotY = np.array([
+        return np.array([
                 [np.cos(theta), 0, np.sin(theta)],
                 [0, 1, 0],
                 [-np.sin(theta), 0, np.cos(theta)]
                 ])
 
+    def rotateY(self, vertex):
+        """
+        From linear algebra we get:
+        (A * B^T)^T = B^T^T * A^T = B * A^T
+        """
+        return np.matmul(vertex, self.create_rotation_matrix_Y(self.thetaY).T)
+
     def create_rotation_matrix_Z(self, theta=0.0):
-        self.rotZ = np.array([
+        return np.array([
                 [np.cos(theta), -np.sin(theta), 0],
                 [np.sin(theta), np.cos(theta), 0],
                 [0, 0, 1]
                 ])
+
+    def rotateZ(self, vertex):
+        """
+        From linear algebra we get:
+        (A * B^T)^T = B^T^T * A^T = B * A^T
+        """
+        return np.matmul(vertex, self.create_rotation_matrix_Z(self.thetaZ).T)
 
     def orthogonal_projection_matrix(self):
         self.projectionZ = np.array([[1, 0, 0],
@@ -77,8 +91,11 @@ class Render():
             self.screen.fill(darkBlue)
 
             for vertex in self.vertices:
-                x, y, z = self.rotateX(vertex)
+                x, y, z = self.rotateY(vertex)
                 self.screen.fill(white, (x, y, 8, 8))
+
             self.thetaX += 0.001
+            self.thetaY += 0.001
+            self.thetaZ += 0.001
 
             pg.display.flip()
