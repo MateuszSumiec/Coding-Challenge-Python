47b3f664697f83ee305af33c5249a5137860d0cb
Mateusz <msumiec@gmail.com>
2019-08-27 18:20:54 +0200

    Separate distance table creation by for loops as new function for readability

--- Levenshtein Distance/levenshtein_distance.py
+++ Levenshtein Distance/levenshtein_distance.py
@@ -25,23 +25,27 @@ def cost_function(first_char: str, second_char: str) -> bool:
     return 0 if (first_char == second_char) else 1
 
 
-def leven_distance(first_string: str, second_string: str) -> int:
-    m = len(first_string) + 1
-    n = len(second_string) + 1
-    table = np.zeros((m, n), dtype=np.int32)
-
-    set_first_row_with_incrementing_integers(table)
-    set_first_column_with_incrementing_integers(table)
-
-    # reversing order of loops
-    for j in range(1, n):
-        for i in range(1, m):
+def create_distance_table(table: np.array, first_string: str, second_string: str) -> None:
+    for j in range(1, len(second_string)+1):
+        for i in range(1, len(first_string)+1):
             table[i, j] = min(table[i-1, j] + 1,
                               table[i, j-1] + 1,
                               table[i-1, j-1] + cost_function(first_string[i-1], 
                                                               second_string[j-1]))
+    return None
+
+
+def leven_distance(first_string: str, second_string: str) -> int:
+    m = len(first_string) + 1
+    n = len(second_string) + 1
+    table = np.zeros((m, n), dtype=np.int32)
+    set_first_row_with_incrementing_integers(table)
+    set_first_column_with_incrementing_integers(table)
+
+    create_distance_table(table, first_string, second_string)
     # because of m, n definition
     return table[m-1, n-1]
 
 
 print(leven_distance('Ala ma psa Farme', 'Ala ma kota Farme'))
+print(leven_distance('Hello World!', 'hello world?'))
